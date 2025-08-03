1a62957e3aad0dd0e9bfdc6a6c82a57c87f9994c
nastassia1409 <adlzhnkv@gmail.com>
2025-08-03 11:41:01 +0300

    small fix

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -10,9 +10,9 @@ public abstract class BaseTest {
 
     public static final String CUSTOMER_CODE = "6703615";
     public static final String CHANGE_TEAM_ENDPOINT = "/customer/changeLicensesTeam";
-    public static final String API_KEY_COMPANY_ADMIN = "9ob1wyrrh28v3zoqe0qx14naf";
-    public static final String API_KEY_TEAM_ADMIN = "virl4tbyf5b74i8d27jlwj0u";
-    public static final String API_KEY_VIEWER = "czmsw4dfcqh0lyqu6yd7fzg3o";
+    public static final String API_KEY_COMPANY_ADMIN = "";
+    public static final String API_KEY_TEAM_ADMIN = "";
+    public static final String API_KEY_VIEWER = "";
 
     @BeforeAll
     static void setup() {
